eb7d649593b61775e46744f242ae741ef94f0230
Joel Ivory Johnson <joel@j2i.net>
2023-06-09 10:25:04 -0400

    Optimizations.

--- FileSystemWatcherSample/ViewModels/MainViewModel.cs
+++ FileSystemWatcherSample/ViewModels/MainViewModel.cs
@@ -14,25 +14,36 @@ namespace FileSystemWatcherSample.ViewModels
         public MainViewModel() {
 
 
-            var assemblyFile = new FileInfo(this.GetType().Assembly.Modules.FirstOrDefault().FullyQualifiedName);
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            FileInfo assemblyFile = new(GetType().Assembly.Modules.FirstOrDefault().FullyQualifiedName);
             var parentDirectory = assemblyFile.Directory;
             Directory.SetCurrentDirectory(parentDirectory.FullName);
-
-
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
             ReloadTitle(Path.Combine(parentDirectory.FullName, "title.txt"));
             ReloadContent(Path.Combine(parentDirectory.FullName, "contents.txt"));
-
-
             FileSystemWatcher fsw = new FileSystemWatcher(parentDirectory.FullName);
             fsw.Filter = "*.txt";
             fsw.Created += FswCreatedOrChanged;
             fsw.Changed += FswCreatedOrChanged;
             fsw.NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.LastWrite | NotifyFilters.FileName;
             fsw.EnableRaisingEvents = true;
-
-
         }
 
+        void FswCreatedOrChanged(object sender, FileSystemEventArgs e)
+        {
+            var name = e.Name.ToLower();
+            switch (name)
+            {
+                case "contents.txt":
+                    ReloadContent(e.FullPath);
+                    break;
+                case "title.txt":
+                    ReloadTitle(e.FullPath);
+                    break;
+                default:
+                    break;
+            }
+        }
 
         void ReloadContent(String fullPath)
         {
@@ -57,21 +68,7 @@ namespace FileSystemWatcherSample.ViewModels
                 Title = "<unreadable>";
             }
         }
-        void FswCreatedOrChanged(object sender, FileSystemEventArgs e)
-        {
-            var name = e.Name.ToLower();
-            switch (name)
-            {
-                case "contents.txt":
-                    ReloadContent(e.FullPath);
-                    break;
-                case "title.txt":
-                    ReloadTitle(e.FullPath);
-                    break;
-                default:
-                    break;
-            }
-        }
+
 
         private string _title = "<<empty>>";
         public string Title
